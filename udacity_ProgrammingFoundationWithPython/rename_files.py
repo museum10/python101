1f491563571147094dfca12039479516ded91220
dusskapark <dusskapark@gmail.com>
2015-06-02 01:49:10 +0900

    Python3.x에서 발생하는 문제 수정
    파이썬3 버전 이상에서 발생하는 문제를 수정함.

--- udacity_ProgrammingFoundationWithPython/rename_files.py
+++ udacity_ProgrammingFoundationWithPython/rename_files.py
@@ -7,8 +7,16 @@ def rename_files():
     os.chdir(r"/Users/dusskapark/Documents/workspace/python101/udacity_ProgrammingFoundationWithPython/prank")
     #(2)for - loop 파일명을 모두 수정하자. 
     for file_name in file_list:
-        os.rename(file_name, file_name.translate(None,"0123456789")) 
-        #string.translate()로 숫자를 삭제함.
+        print("Old Name - "+file_name)
+        print("New Name - "+file_name.strip(" 0123456789"))
+        #파이썬2.7의 경우 
+        #os.rename(file_name, file_name.translate(None,"0123456789")) 
+        ##string.translate()로 숫자를 삭제함.
+        
+        #파이썬3의 경우,
+        os.rename(file_name, file_name.strip(" 0123456789"))
+        
+        
     os.chdir(saved_path)
          
 rename_files()
